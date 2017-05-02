9b0efb01ea27118c7715e05c93ff71cd5b6cd2e0
laiis <laiis1981@gmail.com>
2017-05-03 01:27:29 +0800

    updaet Demo

--- app/src/main/java/tw/idv/laiis/ezretrofitdemo/EZRetrofitApp.java
+++ app/src/main/java/tw/idv/laiis/ezretrofitdemo/EZRetrofitApp.java
@@ -1,11 +1,17 @@
 package tw.idv.laiis.ezretrofitdemo;
 
 import android.app.Application;
+import android.content.Context;
 
-import java.net.CookieManager;
+import java.io.IOException;
 
+import okhttp3.Authenticator;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.Route;
 import tw.idv.laiis.ezretrofit.EZRetrofit;
 import tw.idv.laiis.ezretrofit.RetrofitConf;
+import tw.idv.laiis.ezretrofit.managers.EZRetrofitCookieManager;
 
 /**
  * Created by laiis on 2017/4/30.
@@ -28,9 +34,17 @@ public class EZRetrofitApp extends Application {
 
     public static void initialEZRetrofit() {
         // I want to use this pattern to call api.
+        EZRetrofitCookieManager.newInstance().initial(sEZRetrofitApp, sEZRetrofitApp.getSharedPreferences("test", Context.MODE_PRIVATE));
         EZRetrofit.initial(new RetrofitConf.Builder(getEZRetrofitApp())
-                .setCookieHandler(CookieManager.getDefault())
+                .setCookieHandler(EZRetrofitCookieManager.newInstance().getCookieManager())
                 .timeout(15L)
+                .setAuthenticator(new Authenticator() {
+                    @Override
+                    public Request authenticate(Route route, Response response) throws IOException {
+                        return null;
+                    }
+                })
+                .setRetryOnConnectionFailure(true)
                 .baseUrls(JsonWebservice.class, "http://data.taipei/")
                 .build());
     }
